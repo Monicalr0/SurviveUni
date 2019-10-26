8690e1818da1dda6509e60ae8cb7cf9539aae586
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2019-10-26 16:55:38 -0400

    Added SleepGameActivity.java & activity_sleep_game.xml

---
+++ phase1/SurviveUni/app/src/main/java/com/example/surviveuni/SleepGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.surviveuni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_game);
+    }
+}
