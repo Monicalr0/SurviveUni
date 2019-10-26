1de2032f9e76327aebdc74edadacc46e1729af1a
Chang Yuan <chang.yuan@mail.utoronto.ca>
2019-10-26 15:58:10 -0400

    Added SheepMainActivity.java & activity_sheep_main.xml

---
+++ phase1/SurviveUni/app/src/main/java/com/example/surviveuni/SheepMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.surviveuni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SheepMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheep_main);
+    }
+}
