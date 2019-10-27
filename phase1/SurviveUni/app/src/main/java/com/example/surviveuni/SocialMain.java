90fda2330d8b035e8d57463947d6643b0ba8f25f
Hongbo Zhou <eleanor.zhou@mail.utoronto.ca>
2019-10-27 18:14:02 -0400

    "Created SocialMain for main activity of the Social game and created the activity_social_main.xml for the layout."

---
+++ phase1/SurviveUni/app/src/main/java/com/example/surviveuni/SocialMain.java
@@ -0,0 +1,20 @@
+package com.example.surviveuni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SocialMain extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_main);
+    }
+
+    public void StartSleepGame(View view){
+        Intent startGame = new Intent(this, SocialActivity.class);
+        startActivity(startGame);
+    }
+}
