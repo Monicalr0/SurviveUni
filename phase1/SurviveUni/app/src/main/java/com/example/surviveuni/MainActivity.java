2cbe525d8c3c087239604384de3decbcdf3b03b5
Hongbo Zhou <eleanor.zhou@mail.utoronto.ca>
2019-10-28 11:53:38 -0400

    "Created rough MainActivity layout to be adjusted later."

--- phase1/SurviveUni/app/src/main/java/com/example/surviveuni/MainActivity.java
+++ phase1/SurviveUni/app/src/main/java/com/example/surviveuni/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.surviveuni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void StartSleep(View view){
+        Intent startGame = new Intent(this, SleepMainActivity.class);
+        startActivity(startGame);
+    }
+
+    public void StartSocial(View view){
+        Intent startGame = new Intent(this, SocialActivity.class);
+        startActivity(startGame);
+    }
+
+    public void StartStudy(View view){
+        Intent startGame = new Intent(this, StudyMenu.class);
+        startActivity(startGame);
+    }
+
 }
