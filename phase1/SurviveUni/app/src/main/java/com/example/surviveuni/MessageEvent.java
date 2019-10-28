baaf5fcb9f730ef3f83ed5697d6397efe4393cea
SHAOXUAN ZHENG <shaoxuan.zheng@mail.utoronto.ca>
2019-10-28 11:18:07 -0400

    Add study activity main menu

--- phase1/SurviveUni/app/src/main/java/com/example/surviveuni/MessageEvent.java
+++
@@ -1,7 +0,0 @@
-package com.example.surviveuni;
-
-class MessageEvent extends SuperEvent {
-    public MessageEvent(int x,int y) {
-        super(x,y,2);
-    }
-}
--- phase1/SurviveUni/app/src/main/java/com/example/surviveuni/PassingbyEvent.java
+++
@@ -1,7 +0,0 @@
-package com.example.surviveuni;
-
-class PassingbyEvent extends SuperEvent {
-    public PassingbyEvent(int x, int y, double time_left) {
-        super(x, y, time_left);
-    }
-}
