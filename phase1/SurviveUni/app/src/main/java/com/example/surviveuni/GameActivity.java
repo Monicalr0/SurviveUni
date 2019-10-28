12657adcf14445b47ff73e28b339115746bcf60d
Hongbo Zhou <eleanor.zhou@mail.utoronto.ca>
2019-10-28 13:21:41 -0400

    "Created GameActivity and rough layout."

---
+++ phase1/SurviveUni/app/src/main/java/com/example/surviveuni/GameActivity.java
@@ -0,0 +1,32 @@
+package com.example.surviveuni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void StartSleep(View view){
+        Intent startGame = new Intent(this, SleepMainActivity.class);
+        startActivity(startGame);
+    }
+
+    public void StartSocial(View view){
+        Intent startGame = new Intent(this, SocialActivity.class);
+        startActivity(startGame);
+    }
+
+    public void StartStudy(View view){
+        Intent startGame = new Intent(this, StudyMenu.class);
+        startActivity(startGame);
+    }
+
+}
