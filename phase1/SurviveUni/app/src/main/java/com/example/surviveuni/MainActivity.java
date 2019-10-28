f83899e5359a3a50fecd3b90839834def6ac3702
Chang Yuan <chang.yuan@mail.utoronto.ca>
2019-10-28 19:30:34 -0400

    modify MainActivity.java

--- phase1/SurviveUni/app/src/main/java/com/example/surviveuni/MainActivity.java
+++ phase1/SurviveUni/app/src/main/java/com/example/surviveuni/MainActivity.java
@@ -2,14 +2,10 @@ package com.example.surviveuni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.UserManager;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.View;
 
-import java.util.InputMismatchException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,34 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UserManager = new UserManager(this);
-        setLogInBtn();
     }
 
-    private void setLogInBtn(){
-        EditText usernameInput = findViewById(R.id.MainUserNameInput);
-        EditText passwordInput = findViewById(R.id.MainPassWordInput);
-        Button loginBtn = findViewById(R.id.MainLogInBtn);
+    public void LogIn(View view) {
+        Intent logIn = new Intent(this, LoginActivity.class);
+        startActivity(logIn);
+    }
 
-        loginBtn.setOnClickListener((v) -> {
-
-            try {
-                User user = userManager.authenticate(usernameInput.getText().toString(),
-                        passwordInput.getText().toString());
-
-                Intent i = new Intent(this, GameMenu.class);
-                i.putExtra("USER", user);
-                startActivity(i);
-
-            }
-            catch (InputMismatchException e){
-                new AlertDialog.Builder(this)
-                        .setTitle("Wrong Input")
-                        .setMessage("username or password is wrong")
-                        .setPositiveButton(android.R.string.yes, null)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
-            }
-        });
+    public void createAccount(View view) {
+        Intent createAcc = new Intent(this, CreateAccountActivity.class);
+        startActivity(createAcc);
     }
 }
