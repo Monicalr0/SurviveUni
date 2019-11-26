fdb2e9bd9979dec6b456ec7b395311b3f5d7de51
Minyi Ma <mmymonicalr@gmail.com>
2019-11-26 12:07:41 -0500

    "Add level choice for social"

--- phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/SocialMain.java
+++ phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/SocialMain.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.surviveuni.data.User;
 import com.example.surviveuni.gameCentre.GameActivity;
@@ -12,10 +14,21 @@ import com.example.surviveuni.R;
 
 public class SocialMain extends AppCompatActivity {
     private User user;
+
+    /**
+     * The list of choices for the spinner that allows the user to choose which
+     * Java feature to demonstrate.
+     */
+    private final String[] levels = {"EASY", "NORMAL", "HARD"};
+    private Spinner spinner;
+    private String levelChoice;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_main);
+        setLevelSpinner();
 
         //get user here
         Intent i = getIntent();
@@ -26,6 +39,7 @@ public class SocialMain extends AppCompatActivity {
     }
 
     public void StartSocialGame(View view) {
+        levelChoice = spinner.getSelectedItem().toString();
         Intent startGame = new Intent(this, SocialActivity.class);
         startGame.putExtra("User",user);
         startActivity(startGame);
@@ -35,5 +49,15 @@ public class SocialMain extends AppCompatActivity {
         Intent exitGame = new Intent(this, GameActivity.class);
         exitGame.putExtra("User",user);
         startActivity(exitGame);
+    }
+
+    /**
+     * Set up the gameLevel spinner
+     **/
+    public void setLevelSpinner() {
+        spinner = findViewById(R.id.gameLevel);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, levels);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
