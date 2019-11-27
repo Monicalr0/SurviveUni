41326c3504a860ee9f71f9ee0e942c55927918a9
Minyi Ma <mmymonicalr@gmail.com>
2019-11-27 18:04:29 -0500

    Add different ImageView for different feedback feature for social.

--- phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/Socialfeedback.java
+++ phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/Socialfeedback.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.example.surviveuni.data.User;
 import com.example.surviveuni.gameCentre.GameActivity;
@@ -15,10 +16,12 @@ import com.example.surviveuni.data.GameState;
 import com.example.surviveuni.R;
 import com.example.surviveuni.gameCentre.UserManager;
 
+
 public class Socialfeedback extends AppCompatActivity {
     private GameState gameState;
     private User user;
     private UserManager userManager;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +40,24 @@ public class Socialfeedback extends AppCompatActivity {
         String statsFeedback = checkFeedback(feedback);
         TextView textView2 = findViewById(R.id.statsText);
         textView2.setText(statsFeedback);
-    }
+    };
 
     private String checkFeedback(String feedback) {
+        iv = findViewById(R.id.imageView1);
         if (feedback.equals("Correct! Let's be friend!")) {
+            iv.setImageResource(R.drawable.wow);
             gameState.changeGPA(-5);
             gameState.changeSpirit(-5);
             gameState.changeHappiness(10);
             return ("Happiness:+10\nGPA:-5\nSpirit:-5");
         } else if (feedback.equals("Sorry! Maybe next time.")) {
+            iv.setImageResource(R.drawable.sorry);
             gameState.changeGPA(-5);
             gameState.changeSpirit(-5);
             gameState.changeHappiness(-5);
             return ("Happiness:-5\nGPA:-5\nSpirit:-5");
         } else {
+            iv.setImageResource(R.drawable.angry);
             gameState.changeGPA(-5);
             gameState.changeSpirit(-10);
             gameState.changeHappiness(-10);
