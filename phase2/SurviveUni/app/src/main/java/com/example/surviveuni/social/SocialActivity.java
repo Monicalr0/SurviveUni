92db49732510185770e0838b91bdd73194d089b6
Minyi Ma <mmymonicalr@gmail.com>
2019-12-01 16:01:49 -0500

    Fix the bug in social's observable pattern.

--- phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/SocialActivity.java
+++ phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/SocialActivity.java
@@ -1,17 +1,19 @@
 package com.example.surviveuni.social;
 
-import java.util.Random;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.surviveuni.R;
+import com.example.surviveuni.data.GameState;
 import com.example.surviveuni.data.User;
+import com.example.surviveuni.gameCentre.GameManager;
+
+import java.util.Observer;
 
 public class SocialActivity extends AppCompatActivity implements SocialGameView {
     private User user;
@@ -28,6 +30,10 @@ public class SocialActivity extends AppCompatActivity implements SocialGameView 
         String levelSelected = level.getStringExtra(SocialMain.EXTRA_MESSAGE);
         user = (User) level.getSerializableExtra("User");
         social = new Social();
+        GameState gs = GameManager.getGameState();
+        Observer sc = social;
+        gs.addObserver(sc);
+        gs.socialNotify();
         social.setRemainingGuess(levelSelected);
         social.remainingGuess = social.getRemainingGuess();
         setContentView(R.layout.activity_social_answer);
@@ -67,6 +73,5 @@ public class SocialActivity extends AppCompatActivity implements SocialGameView 
     public void setFailedMessage() {
         Toast.makeText(this, "Sorry! Your answer is not even a number.", Toast.LENGTH_SHORT).show();
     }
-
 
 }
