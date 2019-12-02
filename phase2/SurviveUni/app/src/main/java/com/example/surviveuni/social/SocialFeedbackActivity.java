0437d67200e3764a03259f1ddb977df60037463d
Minyi Ma <mmymonicalr@gmail.com>
2019-12-01 20:18:34 -0500

    Delete unnecessary comment.

--- phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/SocialFeedbackActivity.java
+++ phase2/SurviveUni/app/src/main/java/com/example/surviveuni/social/SocialFeedbackActivity.java
@@ -1,31 +1,25 @@
 package com.example.surviveuni.social;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.surviveuni.R;
 import com.example.surviveuni.data.User;
 import com.example.surviveuni.gameCentre.FeedbackActivity;
-import com.example.surviveuni.data.GameState;
-import com.example.surviveuni.R;
 import com.example.surviveuni.gameCentre.UserManager;
 
 
 public class SocialFeedbackActivity extends FeedbackActivity {
     private ImageView iv;
-    private Social social;
     private SocialFeedbackPresenter presenter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_feedback);
-        social = new Social();
         presenter = new SocialFeedbackPresenter();
         Intent intent = getIntent();
         String feedback = intent.getStringExtra(SocialActivity.EXTRA_MESSAGE);
@@ -47,33 +41,6 @@ public class SocialFeedbackActivity extends FeedbackActivity {
                 .setPositiveButton(android.R.string.yes, null)
                 .setIcon(android.R.drawable.ic_dialog_alert);
     }
-
-    ;
-
-//    private String checkFeedback(String feedback) {
-//        gameState.addObserver(social);
-//        gameState.socialNotify();
-//        iv = findViewById(R.id.imageView1);
-//        if (feedback.equals("Correct! Let's be friend!")) {
-//            iv.setImageResource(R.drawable.wow);
-//            gameState.changeGPA(-5);
-//            gameState.changeSpirit(-5);
-//            gameState.changeHappiness(10);
-//            return ("Happiness:+10\nGPA:-5\nSpirit:-5");
-//        } else if (feedback.equals("Sorry! Run out of playing times:( Maybe next time.")) {
-//            iv.setImageResource(R.drawable.sorry);
-//            gameState.changeGPA(-5);
-//            gameState.changeSpirit(-5);
-//            gameState.changeHappiness(-5);
-//            return ("Happiness:-5\nGPA:-5\nSpirit:-5");
-//        } else {
-//            iv.setImageResource(R.drawable.angry);
-//            gameState.changeGPA(-5);
-//            gameState.changeSpirit(-10);
-//            gameState.changeHappiness(-10);
-//            return ("Happiness:-10\nGPA:-5\nSpirit:-10");
-//        }
-//    }
 
     private String checkFeedback() {
         iv = findViewById(R.id.imageView1);
